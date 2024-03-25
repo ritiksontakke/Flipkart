ded5843dd7c5777891d59ab3814c39f4f4630a44
Ritik Arun Sontakke <ritiksontakke1008@gmail.com>
2024-03-25 17:54:43 +0530

    update atlas db url

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 export const Connection = async (username, password) => {
-    const URL = `mongodb://${username}:${password}@ac-kth4vkh-shard-00-00.e6pldoe.mongodb.net:27017,ac-kth4vkh-shard-00-01.e6pldoe.mongodb.net:27017,ac-kth4vkh-shard-00-02.e6pldoe.mongodb.net:27017/?ssl=true&replicaSet=atlas-lzsz90-shard-0&authSource=admin&retryWrites=true&w=majority`
+    const URL= `mongodb+srv://${username}:${password}@ritik.e6pldoe.mongodb.net/?retryWrites=true&w=majority&appName=Ritik`
+    // const URL = `mongodb://${username}:${password}@ac-kth4vkh-shard-00-00.e6pldoe.mongodb.net:27017,ac-kth4vkh-shard-00-01.e6pldoe.mongodb.net:27017,ac-kth4vkh-shard-00-02.e6pldoe.mongodb.net:27017/?ssl=true&replicaSet=atlas-lzsz90-shard-0&authSource=admin&retryWrites=true&w=majority`
     // const URL =`mongodb://${username}:${password}@ac-uc4a0ej-shard-00-00.vplcwlc.mongodb.net:27017,ac-uc4a0ej-shard-00-01.vplcwlc.mongodb.net:27017,ac-uc4a0ej-shard-00-02.vplcwlc.mongodb.net:27017/?ssl=true&replicaSet=atlas-9d2r6q-shard-0&authSource=admin&retryWrites=true&w=majority`
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, });
